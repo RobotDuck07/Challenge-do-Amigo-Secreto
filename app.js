8f877a9efd3b32a57d194b946bd57f727e3aeb02
Carlos Henrique <carloshepro@gmail.com>
2025-09-22 17:57:33 -0300

    Challenge metade do caminho

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeamigos = [];
+
+function adicionarAmigo() {
+
+    let lista =  document.getElementById('amigo').value
+    if(lista.trim()==""){
+        alert("Por favor, insira um nome.");
+        return;
+      }
+ listaDeamigos.push(lista);
+ document.getElementById('amigo').value = "";
+ atualizarLista();
+}
+
+function atualizarLista() {
+  let lista = document.getElementById('listaAmigos');
+  lista.innerHTML = "";
+
+  for (let i = 0; i < listaDeamigos.length; i++) {
+    const li = document.createElement("li");
+    li.textContent = listaDeamigos[i];
+    lista.appendChild(li);
+  }
+}
